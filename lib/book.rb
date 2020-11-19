a512dfc3aa8f836c7fdb33035a5135cee9e61cb7
DronAce <okeynazone@gmail.com>
2020-11-19 18:45:57 -0500

    commit tree 4e5ae7ebf3a5ba53650c946f07d67b5bfddf9803

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,17 @@
 
 
+class Book
+    attr_accessor :author, :page_count, :genre
+    attr_reader :title
+
+    def initialize(title)
+        @title = title
+        @author = nil
+        @page_count = nil
+        @genre = nil
+    end
+    
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end 
